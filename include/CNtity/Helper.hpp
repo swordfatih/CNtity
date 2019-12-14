881ee72c6a557b856f4167e9129f40486014304a
swordfatih <35528737+swordfatih@users.noreply.github.com>
2019-12-14 14:13:38 +0100

    updated license

--- include/CNtity/Helper.hpp
+++ include/CNtity/Helper.hpp
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////////////////////////////////////
 //
 // CNtity - Chats Noirs Entity Component System Helper
-// Copyright (c) 2018 Fatih (accfldekur@gmail.com)
+// Copyright (c) 2018 - 2019 Fatih (accfldekur@gmail.com)
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
